84e783249ad57578d1198803da4193240564e45c
Vanessa Soares de Morais <vsmdantas@outlook.com>
2022-03-17 22:44:21 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo. Projeto no Visual Studio");
+            Console.WriteLine("A execucao acabou. Tecle enter para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
